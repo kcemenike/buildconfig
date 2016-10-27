787674a0e724aa2a7b94181add774c5dcd3ada97
Moritz Moeller <mm@mxs.de>
2016-10-27 12:34:07 +0200

    ..

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'buildconfig',
+  packages = ['buildconfig'],
+  version = '0.1',
+  description = '.buildconfig cli tool',
+  author = 'Moritz Möller',
+  author_email = 'mm@mxs.de',
+  url = 'https://github.com/mo22/buildconfig',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  classifiers = [],
+)
+
